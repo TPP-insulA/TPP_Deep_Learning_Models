6f2bd5b765d980b584cf1eedc15733ae7e52f443
tomasshiao <tomasshiao2001@gmail.com>
2025-04-15 03:05:38 -0300

    fix: TCN model

--- config/params.py
+++ config/params.py
@@ -162,14 +162,14 @@ USE_TF_MODELS = {
 # Modelos JAX a utilizar
 USE_JAX_MODELS = {
     ## Modelos de Aprendizaje Profundo
-    "jax_attention_only": True,
-    "jax_cnn": True,
-    "jax_fnn": True,
-    "jax_gru": True,
-    "jax_lstm": True,
-    "jax_rnn": True,
-    "jax_tabnet": True,
-    "jax_tcn": False,
+    "jax_attention_only": False,
+    "jax_cnn": False,
+    "jax_fnn": False,
+    "jax_gru": False,
+    "jax_lstm": False,
+    "jax_rnn": False,
+    "jax_tabnet": False,
+    "jax_tcn": True,
     "jax_transformer": False,
     "jax_wavenet": False,
     ## Modelos de Aprendizaje por Refuerzo
